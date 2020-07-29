ea97bd6b90f62201a4307057d10e5a213d1f85a6
Ajay-Pratap-Singh <ajay.snp99@gmail.com>
2020-07-29 18:24:24 +0530

    Updates

---
+++ precache-manifest.a26915f3348316513e9dffe431ecc8c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32ea6f37f38e3f8d982f3f7c9101d4de",
+    "url": "/sempiternal-culture/index.html"
+  },
+  {
+    "revision": "2221eaa529350451b802",
+    "url": "/sempiternal-culture/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7145fc9fb7112bb37ab5",
+    "url": "/sempiternal-culture/static/js/2.dd1a9e2c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sempiternal-culture/static/js/2.dd1a9e2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2221eaa529350451b802",
+    "url": "/sempiternal-culture/static/js/main.1020aeb5.chunk.js"
+  },
+  {
+    "revision": "455a60b6e6074ccfeb7d",
+    "url": "/sempiternal-culture/static/js/runtime-main.ae1e152a.js"
+  }
+]);
